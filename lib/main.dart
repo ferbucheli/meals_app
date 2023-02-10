9ea2d758a7ab5fb8e7359624490f8b65c93177aa
ferbucheli <fbucheli@espol.edu.ec>
2023-02-09 22:23:33 -0500

    add meal_detail screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/screens/categories_screen.dart';
 import 'package:meals_app/screens/category_meals_screen.dart';
+import 'package:meals_app/screens/meal_detail_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -36,6 +37,17 @@ class MyApp extends StatelessWidget {
       routes: {
         '/': (ctx) => const CategoriesScreen(),
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+        MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
+      },
+      onGenerateRoute: (settings) {
+        return MaterialPageRoute(
+          builder: (context) => const CategoriesScreen(),
+        );
+      },
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(
+          builder: (context) => const CategoriesScreen(),
+        );
       },
     );
   }
---
+++ lib/screens/meal_detail_screen.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import '../dummy_data.dart';
+
+class MealDetailScreen extends StatelessWidget {
+  static const routeName = '/meal-detail';
+  MealDetailScreen({super.key});
+
+  Widget buildSectionTitle(String title, BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      child: Text(
+        title,
+        style: Theme.of(context).textTheme.headline6,
+      ),
+    );
+  }
+
+  Widget buildContainer(Widget child) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(
+          color: Colors.grey,
+        ),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      margin: const EdgeInsets.all(10),
+      padding: const EdgeInsets.all(10),
+      height: 150,
+      width: 300,
+      child: child,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final mealId = ModalRoute.of(context)!.settings.arguments as String;
+    final selectedMeal = DUMMY_MEALS.firstWhere((meal) => meal.id == mealId);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('${selectedMeal.title}'),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                height: 300,
+                width: double.infinity,
+                child: Image.network(
+                  selectedMeal.imageUrl,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              buildSectionTitle('Ingredients', context),
+              buildContainer(
+                ListView.builder(
+                  itemCount: selectedMeal.ingredients.length,
+                  itemBuilder: (ctx, idx) => Card(
+                    color: Theme.of(context).colorScheme.secondary,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        selectedMeal.ingredients[idx],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              buildSectionTitle('Steps', context),
+              buildContainer(
+                ListView.builder(
+                  itemBuilder: (ctx, idx) => Column(
+                    children: [
+                      ListTile(
+                        leading: CircleAvatar(
+                          child: Text('# ${idx + 1}'),
+                        ),
+                        title: Text(
+                          selectedMeal.steps[idx],
+                        ),
+                      ),
+                      const Divider(),
+                    ],
+                  ),
+                  itemCount: selectedMeal.steps.length,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
